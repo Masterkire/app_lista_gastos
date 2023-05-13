5b940694a0d197da4e81b203b0ff545c4a7db741
Master Erik <davidmasterkire@gmail.com>
2023-05-12 21:09:58 -0600

    prparando entorno de trabajo y terminando de instalar dependencias y webfont

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    // Work+Sans:wght@400;500;700
+    families: ['Work Sans:400,500,700', 'sans-serif']
+  }
+});
+
+const Index = () => {
+  return ( 
+    <App />
+   );
+}
+ 
+export default Index;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
 )
