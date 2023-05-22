c1292306e67d1a021484269e880eba9f43a5963d
Master Erik <davidmasterkire@gmail.com>
2023-05-22 00:40:34 -0600

    Se agrego logica del formulario para validar de lado del cliente

--- src/componentes/RegistroUsuarios.jsx
+++ src/componentes/RegistroUsuarios.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React, {useState} from "react";
 import {Helmet} from 'react-helmet';
 import {Header, Titulo, ContenedorHeader} from './../elementos/Header';
 import {Boton} from './../elementos/Boton';
@@ -6,6 +6,49 @@ import {Formulario, Input, ContenedorBoton} from './../elementos/ElementosDeForm
 import SvgLogin from './../ComponentSvg/SvgLogin';
 
 const RegistroUsuarios = () => {
+    const [correo, establecerCorreo] = useState('');
+    const [password, establecerPassword] = useState('');
+    const [password2, establecerPassword2] = useState('');
+
+    const handleChange = (e) => {
+        switch(e.target.name){
+            case 'email':
+                establecerCorreo(e.target.value);
+                break;
+            case 'password':
+                establecerPassword(e.target.value);
+                break;
+            case 'password2':
+                establecerPassword2(e.target.value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Comprobamos del lado del cliente que el correo sea valido.
+        const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+        if(!expresionRegular.test(correo)){
+            console.log('Por favor ingresa un correo valido');
+            return;
+        }
+
+        if(correo === '' || password === '' || password2 === ''){
+            console.log('Por favor rellena todos los datos');
+            return;
+        }
+
+        if(password != password2){
+            console.log('Las contraseñas no son iguales');
+            return;
+        }
+
+        console.log('Registramos un usuario');
+    }
+
     return ( 
         <>
             <Helmet>
@@ -21,22 +64,28 @@ const RegistroUsuarios = () => {
                 </ContenedorHeader>
             </Header>
 
-            <Formulario>
+            <Formulario onSubmit={handleSubmit}>
                 <SvgLogin />
                 <Input 
                     type="email"
                     name="email"
                     placeholder="Correo Electronico"
+                    value={correo}
+                    onChange={handleChange}
                 />
                 <Input 
                     type="password"
                     name="password"
                     placeholder="Contraseña"
+                    value={password}
+                    onChange={handleChange}
                 />
                 <Input 
                     type="password"
                     name="password2"
                     placeholder="Repetir la contraseña"
+                    value={password2}
+                    onChange={handleChange}
                 />
                 <ContenedorBoton>
                     <Boton as="button" primario type='submit'>Crear Cuenta</Boton>
