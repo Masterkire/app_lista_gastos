60cff350990f377e2098e8f0a15ab4f682952f45
Master Erik <davidmasterkire@gmail.com>
2023-05-21 23:27:31 -0600

    Agregando instalacion de firbase para usar firestore y autenticacion asi como agregando configuracion de variables de entorno para las claves a conexion

--- src/componentes/InicioSesion.jsx
+++ src/componentes/InicioSesion.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+// import React from "react";
+import {Helmet} from 'react-helmet';
+import {Header, Titulo, ContenedorHeader} from './../elementos/Header';
+import {Boton} from './../elementos/Boton';
+import {Formulario, Input, ContenedorBoton} from './../elementos/ElementosDeFormulario';
+import Login from './../ComponentSvg/Login';
 
 const InicioSesion = () => {
     return ( 
-        <h1>Inicio sesion</h1>
+        <>
+            <Helmet>
+                <title>Iniciar sesion</title>
+            </Helmet>
+
+            <Header>
+                <ContenedorHeader>
+                    <Titulo>Iniciar sesion</Titulo>
+                    <div>
+                        <Boton to="/crear-cuenta">Registrarse</Boton>
+                    </div>
+                </ContenedorHeader>
+            </Header>
+
+            <Formulario>
+                <Login />
+                <Input 
+                    type="email"
+                    name="email"
+                    placeholder="Correo Electronico"
+                />
+                <Input 
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                />
+                <ContenedorBoton>
+                    <Boton as="button" primario type='submit'>Iniciar sesion</Boton>
+                </ContenedorBoton>
+            </Formulario>
+        </>
      );
 }
  
---
+++ src/firebase/firebaseConfig.jsx
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import.meta.env;
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+//Congiguracion para tomar las claves desde la variable de entorno en el archivo .env.local usando vite
+const firebaseConfig = {
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export {db, auth};
