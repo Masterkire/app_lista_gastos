aedf6d6c8c94a3eb9509d55993b77836253cd2bc
Master Erik <davidmasterkire@gmail.com>
2023-06-07 23:32:25 -0600

    Agregando gastos a lista de gastos con iconos y botones aun sin funcionalidad

--- src/componentes/ListaDeGastos.jsx
+++ src/componentes/ListaDeGastos.jsx
@@ -5,9 +5,30 @@ import BtnRegresar from './../elementos/BtnRegresar';
 import { useAuth } from "./../contextos/AuthContext";
 import BarraTotalGastado from './BarraTotalGastado';
 import useObtenerGastos from './../hooks/useObtenerGastos';
+import {Lista,
+        ElementoLista,
+        ListaDeCategorias,
+        ElementoListaCategorias,
+        Categoria,
+        Descripcion,
+        Valor,
+        Fecha,
+        ContenedorBotones,
+        BotonAccion,
+        BotonCargarMas,
+        ContenedorBotonCentral,
+        ContenedorSubtitulo,
+        Subtitulo} 
+from './../elementos/ElementosDeLista';
+import IconoCategoria from './../elementos/IconoCategoria';
+import convertirAMoneda from './../funciones/convertirAMoneda';
+import IconoEditar from './../ComponentSvg/Editar';
+import IconoBorrar from './../ComponentSvg/Borrar';
+import {Link} from 'react-router-dom';
+import Boton from './../elementos/Boton';
 
 const ListaDeGastos = () => {
-    const gastos = useObtenerGastos();
+    const [gastos] = useObtenerGastos();
     console.log(gastos);
 
     return ( 
@@ -20,6 +41,44 @@ const ListaDeGastos = () => {
                 <BtnRegresar/>
                 <Titulo>Lista de Gastos</Titulo>
             </Header>
+
+            <Lista>
+                {gastos.map((gasto) => {
+                    return (
+                        <ElementoLista key={gasto.id}>
+                            <Categoria>
+                            <IconoCategoria id={gasto.categoria} />
+                                {gasto.categoria}
+                            </Categoria>
+
+                            <Descripcion>
+                                {gasto.descripcion}
+                            </Descripcion>
+                            <Valor>{convertirAMoneda(gasto.cantidad)}</Valor>
+
+                            <ContenedorBotones>
+                                <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
+                                    <IconoEditar />
+                                </BotonAccion>
+                                <BotonAccion >
+                                    <IconoBorrar />
+                                </BotonAccion>
+                            </ContenedorBotones>
+                        </ElementoLista>
+                    );
+                })}
+
+                <ContenedorBotonCentral>
+                    <BotonCargarMas>Cargar Más</BotonCargarMas>
+                </ContenedorBotonCentral>
+
+                {gastos.length === 0 &&
+                    <ContenedorSubtitulo>
+                        <Subtitulo>No hay gastos por mostrar</Subtitulo>
+                        <Boton as={Link} to={"/"}>Agregar Gasto</Boton>
+                    </ContenedorSubtitulo>
+                }
+            </Lista>
 
             <BarraTotalGastado />
         </>
