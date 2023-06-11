3bd03796ebef95735138fb0dfb37bef581b01bd5
Master Erik <davidmasterkire@gmail.com>
2023-06-10 22:36:49 -0600

    Se agrego fecha y se limito lista gastos a 10 y se agrego boton de cargar más completamente funcional

--- src/componentes/ListaDeGastos.jsx
+++ src/componentes/ListaDeGastos.jsx
@@ -1,14 +1,10 @@
-import React from "react";
 import {Header, Titulo} from './../elementos/Header';
 import {Helmet} from 'react-helmet';
 import BtnRegresar from './../elementos/BtnRegresar';
-import { useAuth } from "./../contextos/AuthContext";
 import BarraTotalGastado from './BarraTotalGastado';
 import useObtenerGastos from './../hooks/useObtenerGastos';
 import {Lista,
         ElementoLista,
-        ListaDeCategorias,
-        ElementoListaCategorias,
         Categoria,
         Descripcion,
         Valor,
@@ -25,11 +21,29 @@ import convertirAMoneda from './../funciones/convertirAMoneda';
 import IconoEditar from './../ComponentSvg/Editar';
 import IconoBorrar from './../ComponentSvg/Borrar';
 import {Link} from 'react-router-dom';
-import Boton from './../elementos/Boton';
+import {Boton} from './../elementos/Boton';
+import {format, fromUnixTime} from 'date-fns';
+import {es} from 'date-fns/locale';
 
 const ListaDeGastos = () => {
-    const [gastos] = useObtenerGastos();
-    console.log(gastos);
+    const [gastos, obtenerMasGastos, hayMasPorCargar] = useObtenerGastos();
+    
+    const formatearFecha = (fecha) => {
+        return format(fromUnixTime(fecha), "dd 'de' MMMM 'de' yyyy", {locale: es})
+    }
+
+    const fechaEsIgual = (gastos, index, gasto) => {
+        if(index != 0){
+            const fechaActual = formatearFecha(gasto.fecha);
+            const fechaGastoAnterior = formatearFecha(gastos[index -1].fecha);
+
+            if(fechaActual === fechaGastoAnterior){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
     return ( 
         <>
@@ -43,34 +57,40 @@ const ListaDeGastos = () => {
             </Header>
 
             <Lista>
-                {gastos.map((gasto) => {
+                {gastos.map((gasto, index) => {
                     return (
-                        <ElementoLista key={gasto.id}>
-                            <Categoria>
-                            <IconoCategoria id={gasto.categoria} />
-                                {gasto.categoria}
-                            </Categoria>
+                        <div key={gasto.id}>
+                            {!fechaEsIgual(gastos, index, gasto) && <Fecha>{formatearFecha(gasto.fecha)}</Fecha>}
+                            
+                            <ElementoLista key={gasto.id}>
+                                <Categoria>
+                                <IconoCategoria id={gasto.categoria} />
+                                    {gasto.categoria}
+                                </Categoria>
 
-                            <Descripcion>
-                                {gasto.descripcion}
-                            </Descripcion>
-                            <Valor>{convertirAMoneda(gasto.cantidad)}</Valor>
+                                <Descripcion>
+                                    {gasto.descripcion}
+                                </Descripcion>
+                                <Valor>{convertirAMoneda(gasto.cantidad)}</Valor>
 
-                            <ContenedorBotones>
-                                <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
-                                    <IconoEditar />
-                                </BotonAccion>
-                                <BotonAccion >
-                                    <IconoBorrar />
-                                </BotonAccion>
-                            </ContenedorBotones>
-                        </ElementoLista>
+                                <ContenedorBotones>
+                                    <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
+                                        <IconoEditar />
+                                    </BotonAccion>
+                                    <BotonAccion >
+                                        <IconoBorrar />
+                                    </BotonAccion>
+                                </ContenedorBotones>
+                            </ElementoLista>
+                        </div>
                     );
                 })}
 
-                <ContenedorBotonCentral>
-                    <BotonCargarMas>Cargar Más</BotonCargarMas>
-                </ContenedorBotonCentral>
+                {hayMasPorCargar &&
+                    <ContenedorBotonCentral>
+                        <BotonCargarMas onClick={() => obtenerMasGastos()}>Cargar Más</BotonCargarMas>
+                    </ContenedorBotonCentral>
+                }
 
                 {gastos.length === 0 &&
                     <ContenedorSubtitulo>
--- src/hooks/useObtenerGastos.jsx
+++ src/hooks/useObtenerGastos.jsx
@@ -1,11 +1,39 @@
 import {useState, useEffect} from 'react';
 import {db} from './../firebase/firebaseConfig';
 import {useAuth} from './../contextos/AuthContext';
-import { collection, onSnapshot, query, orderBy, where, limit } from 'firebase/firestore';
+import { collection, onSnapshot, query, orderBy, where, limit, startAfter } from 'firebase/firestore';
 
 const useObtenerGastos = () => {
     const {usuario} = useAuth();
     const [gastos, cambiarGastos] = useState([]);
+    const [ultimoGasto, cambiarUltimoGasto] = useState(null);
+    const [hayMasPorCargar, cambiarHayMasPorCargar] = useState(false);
+
+    const obtenerMasGastos = () => {
+        const consulta = query(
+            collection(db, 'gastos'),
+            where('uidUsuario', '==', usuario.uid),
+            orderBy('fecha', 'desc'),
+            limit(10),
+            startAfter(ultimoGasto)
+        );
+
+        onSnapshot(consulta, (snapshot) => {
+            console.log(snapshot.docs.length);
+            if (snapshot.docs.length > 0) {
+                cambiarUltimoGasto(snapshot.docs[snapshot.docs.length -1]);
+                
+                cambiarGastos(gastos.concat(snapshot.docs.map((gasto) => {
+                    return {...gasto.data(), id: gasto.id}
+                })))
+            } else {
+                console.log("La condicion no es valida");
+                cambiarHayMasPorCargar(false);
+            }
+        }, error => {console.log(error)})
+
+        console.log("Los datos fueron ejecutados");
+    }
 
     useEffect(() => {
         const consulta = query(
@@ -16,15 +44,23 @@ const useObtenerGastos = () => {
         );
 
         const unsuscribe = onSnapshot(consulta, (snapshot) => {
+            /* console.log(snapshot.docs[snapshot.docs.length -1].data()); */
+            if (snapshot.docs.length > 0) {
+                cambiarUltimoGasto(snapshot.docs[snapshot.docs.length -1]);
+                cambiarHayMasPorCargar(true);
+            } else {
+                cambiarHayMasPorCargar(false);
+            }
+
             cambiarGastos(snapshot.docs.map((gasto) => {
                 return {...gasto.data(), id: gasto.id}
             }));
-        })
+        });
 
         return unsuscribe;
     }, [usuario]);
 
-    return [gastos];
+    return [gastos, obtenerMasGastos, hayMasPorCargar];
 }
  
 export default useObtenerGastos;
